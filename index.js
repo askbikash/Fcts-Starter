6dd8910ef258a4174140681d781952eaa458fceb
Bikash <bikash.k3044@gmail.com>
2023-12-07 15:53:19 +0530

    custom navbar

--- index.js
+++ index.js
@@ -1,9 +1,63 @@
-var currentPageClass = document.body.className;
+// document.addEventListener("DOMContentLoaded", function () {
+//   // Get all menu items
+//   var menuItems = document.querySelectorAll('.menu li');
 
-var navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+//   // Add click event listener to each menu item
+//   menuItems.forEach(function (menuItem) {
+//     menuItem.addEventListener('click', function () {
+//       // Remove "active" class from all menu items
+//       menuItems.forEach(function (item) {
+//         item.classList.remove('active');
+//       });
 
-navLinks.forEach(function (navLink) {
-  if (navLink.getAttribute('href') === currentPageClass) {
-    navLink.parentNode.classList.add('active');
-  }
+//       // Add "active" class to the clicked menu item
+//       menuItem.classList.add('active');
+//     });
+//   });
+// });
+
+
+// javascript for toggle 
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Get the menu toggle button and menu
+  var menuToggle = document.querySelector('.menu-toggle');
+  var menu = document.querySelector('.menu');
+
+  // Toggle the 'active' class on click
+  menuToggle.addEventListener('click', function () {
+    menu.classList.toggle('active');
+  });
+
+  // Get all menu items
+  var menuItems = document.querySelectorAll('.menu li');
+
+  // Add click event listener to each menu item
+  menuItems.forEach(function (menuItem) {
+    menuItem.addEventListener('click', function (event) {
+      // Remove "active" class from all menu items
+      menuItems.forEach(function (item) {
+        item.classList.remove('active');
+      });
+
+      // Add "active" class to the clicked menu item
+      menuItem.classList.add('active');
+
+      // If the menu is open (on small screens), close it
+      if (menu.classList.contains('active')) {
+        menu.classList.remove('active');
+      }
+    });
+  });
+
+  // Get all submenu items
+  var submenuItems = document.querySelectorAll('.submenu a');
+
+  // Add click event listener to each submenu item
+  submenuItems.forEach(function (submenuItem) {
+    submenuItem.addEventListener('click', function (event) {
+      // Prevent the submenu from closing when clicking on it
+      event.stopPropagation();
+    });
+  });
 });
